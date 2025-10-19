d4d5ab852a8249548ba580c5068ec0929f97740e
Ittiwat Chuchoet <ciaabcdefg@gmail.com>
2025-10-19 19:59:41 +0700

    remove env and new main

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 use anyhow::Result;
 use axum::Router;
-use medbook_core::bootstrap::bootstrap;
+use diesel_migrations::{EmbeddedMigrations, embed_migrations};
+use medbook_core::{
+    bootstrap::{self, bootstrap},
+    config, db,
+};
 use medbook_orderservice::{consumers, routes};
+
+/// Migrations embedded into the binary which helps with streamlining image building process
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    bootstrap::init_tracing();
+    bootstrap::init_env();
+
     let app = Router::new()
         .merge(routes::patients::orders::routes())
         .merge(routes::patients::carts::routes())
         .merge(routes::payments::routes());
 
+    tracing::info!("Running migrations...");
+    let config = config::load()?;
+    let migrations_count = db::run_migrations_blocking(MIGRATIONS, &config.database.url).await?;
+    tracing::info!("Run {} new migrations successfully", migrations_count);
+
+    tracing::info!("Bootstrapping...");
     bootstrap(
         "OrderService",
         app,
